aefcc3baad50ed66e049fa21870b98ef30d56138
AbbasAgashi <abbasagashi53@gmail.com>
2026-09-16 14:58:05 +0530

    changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const app = require("./app"),
   db = require("./config/database");
 const port = process.env.PORT || 5000;
 db()
-  .then(() => app.listen(port, () => console.log(`Server running on ${port}`)))
+  .then(() => app.listen(port,"0.0.0.0",() => console.log(`Server running on ${port}`)))
   .catch((e) => {
     console.error(e);
     process.exit(1);
